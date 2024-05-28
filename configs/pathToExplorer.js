4bce24ad7bdfe4235a28d7d623c4edd1c753ead2
xMarioInc <lchaosmachinel@gmail.com>
2024-05-28 21:15:44 +0300

    reworked path to explorer

--- configs/pathToExplorer.js
+++ configs/pathToExplorer.js
@@ -1,1 +1,9 @@
-export const pathToExplorer = import.meta.env.VITE_TESTNET ? 'https://testnetexplorer.obyte.org' : 'https://explorer.obyte.org'; 
+const getPathToExplorer = () => {
+  if(import.meta.env.VITE_DEV_PATH) {
+    return import.meta.env.VITE_PATH_TO_DEV_SERVER;
+  }
+  
+  return import.meta.env.VITE_TESTNET ? 'https://testnetexplorer.obyte.org' : 'https://explorer.obyte.org';
+}
+
+export const pathToExplorer = getPathToExplorer();
